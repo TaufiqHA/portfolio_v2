2c839feadecf32952e3ebddde976974246304f8f
taufiq <htaufiq225@gmail.com>
2025-03-15 21:27:14 +0800

    setup responsive design

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// hamburger menu
+const hamburger_menu = document.querySelector("#hamburger-menu");
+
+hamburger_menu.addEventListener("click", function () {
+  hamburger_menu.classList.toggle("hamburger-toggle");
+});
