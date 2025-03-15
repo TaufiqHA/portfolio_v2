525d65df5e5760786bbc81ecfc6083cd6d291c7e
taufiq <htaufiq225@gmail.com>
2025-03-16 05:35:48 +0800

    menambahkan responsive design untuk perangkat mobile

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // hamburger menu
 const hamburger_menu = document.querySelector("#hamburger-menu");
+const menu = document.querySelector("#menu");
 
 hamburger_menu.addEventListener("click", function () {
   hamburger_menu.classList.toggle("hamburger-toggle");
+  menu.classList.toggle("show-menu");
 });
